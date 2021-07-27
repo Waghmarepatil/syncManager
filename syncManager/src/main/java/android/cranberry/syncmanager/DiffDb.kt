22ac07693bcf3af8ba9b9308a991e50e801eeb6a
pwaghmarepatil <pwaghmarepatil@gmail.com>
2021-07-27 18:37:03 +0530

    commit 2

--- syncManager/src/main/java/android/cranberry/syncmanager/DiffDb.kt
+++ syncManager/src/main/java/android/cranberry/syncmanager/DiffDb.kt
@@ -3,7 +3,6 @@ package android.cranberry.syncmanager
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import javax.annotation.Nonnull
 
 /**
  * @Author: Pramod Jyotiram Waghmare
@@ -13,19 +12,12 @@ import javax.annotation.Nonnull
 @Entity
 data class DiffDb(
     @PrimaryKey(autoGenerate = true) var id: Byte,
-    @Nonnull
     @ColumnInfo(name = "command") var command: String?,
-    @Nonnull
     @ColumnInfo(name = "table") var table: String?,
-    @Nonnull
     @ColumnInfo(name = "field") var field: String?,
-    @Nonnull
     @ColumnInfo(name = "status") var status: Char?,
-    @Nonnull
     @ColumnInfo(name = "keepOffline", defaultValue = false.toString()) var keepOffline: Boolean?,
-    @Nonnull
     @ColumnInfo(name = "priority") var priority: Byte?,
-    @Nonnull
     @ColumnInfo(name = "payload") var payload: String,
 ){
     override fun toString(): String {
